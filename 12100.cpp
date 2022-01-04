f79ca72e8c4ab61b3b547d8db912b2e3d8ff1adb
정환주 <pp10202@naver.com>
2022-01-04 11:01:42 +0900

    Update 12100.cpp
    개선된 121200
    구조체 내부의 함수 사용으로 가독성 증가
    rotate 함수를 이용해서 push 함수로 4가지 방향 처리 가능

--- 12100.cpp
+++ 12100.cpp
@@ -3,255 +3,87 @@
 #include <iostream>
 #include <algorithm>
 #include <stdlib.h>
+#include <memory.h>
+
 
 using namespace std;
 
 int N, max_num = 1;
-int** arr;
+int** arr, i, j;
 
-void DFS(int** arr, int cnt);
+struct board {
+	int board[20][20];
 
-void DFS(int** arr, int cnt) {
-	// 4가지 방향에 대한 시도
-	
-	int i, j, a, b, tmp = 0;
-	int row = 0, col = 0, k;
-	
-	int** arr2;
-	arr2 = (int**)malloc(sizeof(int*) * N);
-	for (i = 0; i < N; i++)
-		arr2[i] = (int*)malloc(sizeof(int) * N);
-
-
-	if (cnt == 5) {
-		//printf("\n");
+	void rotate() {
+		int tmp[20][20];
 		for (i = 0; i < N; i++) {
 			for (j = 0; j < N; j++) {
-				if (tmp < arr[i][j]) tmp = arr[i][j];
-				//printf("%d ", arr[i][j]);
-			}	
-			//printf("\n");
+				tmp[i][j] = board[N - 1 - j][i];
+			}
 		}
-		max_num = max(max_num, tmp);
-		
-		return ;
+		memcpy(board, tmp, sizeof(board));
 	}
 
+	void push() {
+		int tmp[20][20];
+		for (i = 0; i < N; i++) {
+			int idx = -1, flag = 0;
+			for (j = 0; j < N; j++) {
+				if (board[i][j] == 0);
+				else if (flag && board[i][j] == tmp[i][idx]) {
+					tmp[i][idx] *= 2; flag = 0;
+				}
+				else {
+					tmp[i][++idx] = board[i][j]; flag = 1;
+				}
+			}
+			for (idx++; idx < N; idx++) tmp[i][idx] = 0;
+		}
+		memcpy(board, tmp, sizeof(board));
+	}
+
+	void max_value() {
+		for (i = 0; i < N; i++) {
+			for (j = 0; j < N; j++) {
+				if (max_num < board[i][j]) max_num = board[i][j];
+			}
+		}
+	}
+
+};
 
 
+void DFS(board c, int cnt) {
+
+	if (cnt == 5) {
+		c.max_value();
+		return;
+	}
+
+	int i;
 	for (i = 0; i < 4; i++) {
-		
-		for (a = 0; a < N; a++) {
-			for (b = 0; b < N; b++) {
-				arr2[a][b] = arr[a][b];
-			}
-		}
-
-		switch (i) {
-		case 0:
-			// 올라가는 방향
-			for (col = 0; col < N; col++) {
-				k = 0;
-				while (k < N - 1) {
-					// 일단 다 올려
-					// 검사하는 칸 빈칸이면 다 올리기
-					if (arr2[k][col] == 0) {
-						a = 0;
-						for (j = k; j < N; j++) {
-							if (arr2[j][col] != 0) {
-								a = j - k; break;
-							}
-						}
-						for (j = k; j < N - a; j++) {
-							arr2[j][col] = arr2[j + a][col];
-							arr2[j + a][col] = 0;
-						}
-						arr2[N-1][col] = 0;
-					}
-					k++;
-				}
-			}
-
-			for (col = 0; col < N; col++) {
-				k = 0;
-				while (k < N - 1) {
-					// 검사하는 칸 빈칸(합친 칸)이면 올리기
-					if (arr2[k][col] == 0) {
-						if (arr2[k + 1][col] == 0) break;
-						for (j = k; j < N - 1; j++) {
-							arr2[j][col] = arr2[j + 1][col];
-						}
-						arr2[N - 1][col] = 0;
-						continue;
-					}
-					if (arr2[k][col] == arr2[k + 1][col]) {
-						// 만약 둘이 같으면?
-						// 둘을 합치고 없어진 칸을 0으로 설정
-						arr2[k][col] = 2 * arr2[k][col];
-						arr2[k + 1][col] = 0;
-					}
-					k++;
-				}
-			}
-			break;
-		case 1:
-			// 내려가는 방향
-			for (col = 0; col < N; col++) {
-				k = N - 1;
-				while (k > 0) {
-					if (arr2[k][col] == 0) {
-						a = 0;
-						for (j = k; j >= 0 ; j--) {
-							if (arr2[j][col] != 0) {
-								a = k - j; break;
-							}
-						}
-						for (j = k; j >= a; j--) {
-							arr2[j][col] = arr2[j - a][col];
-							arr2[j - a][col] = 0;
-						}
-
-						//for (j = k; j > 0; j--) 
-						//	arr2[j][col] = arr2[j - 1][col];
-						
-						arr2[0][col] = 0;
-					}
-					k--;
-				}
-			}
-			
-			for (col = 0; col < N; col++) {
-				k = N - 1;
-				while (k > 0) {
-					if (arr2[k][col] == 0) {
-						if (arr2[k - 1][col] == 0) break;
-						for (j = k; j > 0; j--) {
-							arr2[j][col] = arr2[j - 1][col];
-						}
-						arr2[0][col] = 0;
-						continue;
-					}
-					if (arr2[k][col] == arr2[k - 1][col]) {
-						// 둘이 같으면?
-						// 둘을 합치고, 칸을 0으로 설정
-						arr2[k][col] = 2 * arr2[k][col];
-						arr2[k - 1][col] = 0;
-					}
-					k--;
-				}
-			}
-			break;
-		case 2:
-			// 왼쪽으로 미는 경우
-			for (row = 0; row < N; row++) {
-				k = 0;
-				while (k < N - 1) {
-					if (arr2[row][k] == 0) {
-						a = 0;
-						for (j = k; j < N; j++) {
-							if (arr2[row][j] != 0) {
-								a = j - k; break;
-							}
-						}
-						for (j = k; j < N - a; j++) {
-							arr2[row][j] = arr2[row][j + a];
-							arr2[row][j + a] = 0;
-						}
-						arr2[row][N - 1] = 0;
-					}
-					k++;
-				}
-			}
-
-
-			for (row = 0; row < N; row++) {
-				k = 0;
-				while (k < N - 1) {
-					if (arr2[row][k] == 0) {
-						if (arr2[row][k + 1] == 0) break;
-						for (j = k; j < N-1; j++) {
-							arr2[row][j] = arr2[row][j + 1];
-							arr2[row][j + 1] = 0;
-						}
-						arr2[row][N - 1] = 0;
-						continue;
-					}
-					if (arr2[row][k] == arr2[row][k + 1]) {
-						arr2[row][k] = 2* arr2[row][k];
-						arr2[row][k + 1] = 0;
-					}
-					k++;
-				}
-			}
-			break;
-		case 3:
-			// 오른쪽으로 미는 경우
-			for (row = 0; row < N; row++) {
-				k = N - 1;
-				while (k > 0) {
-					if (arr2[row][k] == 0) {
-						a = 0;
-						for (j = k; j >= 0; j--) {
-							if (arr2[row][j] != 0) {
-								a = k - j; break;
-							}
-						}
-						for (j = k; j >= a; j--) {
-							arr2[row][j] = arr2[row][j-a];
-							arr2[row][j - a] = 0;
-						}
-
-						//for (j = k; j > 0; j--) 
-						//	arr2[row][j] = arr2[row][j - 1];
-						
-						arr2[row][0] = 0;
-					}
-					k--;
-				}
-			}
-			for (row = 0; row < N; row++) {
-				k = N - 1;
-				while (k > 0) {
-					if (arr2[row][k] == 0) {
-						if (arr2[row][k - 1] == 0) break;
-						for (j = k; j > 0; j--) {
-							arr2[row][j] = arr2[row][j - 1];
-						}
-						arr2[row][0] = 0;
-						continue;
-					}
-					if (arr2[row][k] == arr2[row][k - 1]) {
-						arr2[row][k] = 2 * arr2[row][k - 1];
-						arr2[row][k - 1] = 0;
-					}
-					k--;
-				}
-			}
-			break;
-		}
-		DFS(arr2, cnt + 1);
+		board next = c;
+		next.push();
+		DFS(next, cnt + 1);
+		c.rotate();
 	}
 }
 
 int main() {
 
 	int i, j;
+	board input;
 
 	cin >> N;
 
-	arr = (int**)malloc(sizeof(int*) * N);
-	for (i = 0; i < N; i++)
-		arr[i] = (int*)malloc(sizeof(int) * N);
-	getchar();
-
 	for (i = 0; i < N; i++) {
 		for (j = 0; j < N; j++) {
-			scanf("%d", &arr[i][j]);
+			scanf("%d", &input.board[i][j]);
 		}
 		getchar();
 	}
 
-	DFS(arr, 0);
+	DFS(input, 0);
 
 	printf("%d\n", max_num);
 
